c556adf74f57068a6c7795e906bba9d52635d9d3
Nilson Jorge Leão Júnior <116318343+NilsonJorge@users.noreply.github.com>
2024-07-27 22:29:28 -0300

    search

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -4,11 +4,12 @@ import { MomentService } from './../../../services/moment.service';
 import { Moment } from './../../../Moment';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { CommonModule } from '@angular/common';
-import {RouterModule} from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FontAwesomeModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
@@ -18,6 +19,8 @@ export class HomeComponent {
   baseApiUrl = 'http://127.0.0.1:3333/';
 
   //todo search
+  faSearch = faSearch;
+  searchTerm: string = '';
 
   constructor(private momentService: MomentService) {}
 
@@ -27,10 +30,17 @@ export class HomeComponent {
 
       data.map((item: any) => {
         // item.created_at = new Date(item.created_at).toLocaleDateString('pt-BR');
-
       });
       this.allMoments = data;
       this.moments = data;
     });
+  }
+  search(e: Event): void {
+    const target = e.target as HTMLInputElement;
+    const value = target.value;
+
+    this.moments = this.allMoments.filter((moment) =>
+      moment.title.toLowerCase().includes(value)
+    );
   }
 }
